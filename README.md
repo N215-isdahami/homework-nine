39bb853d6c4ad290d455a51245313c227cbda0d9
isdahami <isdahami@iu.edu>
2022-04-11 12:48:37 -0400

    init

--- README.md
+++ README.md
@@ -1,13 +1,12 @@
-# homework-eight
+# homework-nine
 
 ##
 
 This homework assignment will:
 
-Teach you how to add jQuery to your page
-Teach you how to add variables to your JS file
-How to use variables
-Write a function
-Interact with HTML elements with jQuery
+Teach you how to set up a simple MVC project
+Teach you how to interact with another js file
+Display information on a page.
+Interact with elements with jQuery
 
-link: https://in-info-web4.informatics.iupui.edu/~isdahami/homework-eight/index.html
+link: https://in-info-web4.informatics.iupui.edu/~isdahami/homework-nine/index.html
---
+++ model/model.js
@@ -0,0 +1,146 @@
+var homeContent = `<div class="hero"></div>
+
+<div class="content">
+    <div class="heroimg"></div>
+    <h1>NOW SHOWING:</h1>
+    <div class="products">
+        <div class="product">
+            <div class="prod-image"></div>
+            <div class="prod-desc">
+                <h3>Breathless (1960)</h3>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad accusantium vero, voluptates dolore rem iusto culpa! Eveniet corrupti dolore doloribus porro distinctio at nulla nesciunt iusto quae eaque. Aspernatur iste quisquam mollitia minima explicabo earum, delectus veritatis, reiciendis aperiam quo magnam distinctio beatae, nobis cupiditate iusto sed optio maiores eius.</p>
+                <h3>SHOWTIMES:</h3>
+                <div class="btn-showTime">12:00pm</div>
+                <div class="btn-showTime">3:30pm</div>
+                <div class="btn-showTime">5:45pm</div>
+                <div class="btn-showTime">7:30pm</div>
+                
+        </div>
+            </div>
+            <div class="product">
+                <div class="prod-image-two"></div>
+                <div class="prod-desc">
+                    <h4>Mon Oncle (1958)</h4>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad accusantium vero, voluptates dolore rem iusto culpa! Eveniet corrupti dolore doloribus porro distinctio at nulla nesciunt iusto quae eaque. Aspernatur iste quisquam mollitia minima explicabo earum, delectus veritatis, reiciendis aperiam quo magnam distinctio beatae, nobis cupiditate iusto sed optio maiores eius.</p>
+                    <h4>SHOWTIMES:</h4>
+                    <div class="btn-showTime">12:00pm</div>
+                    <div class="btn-showTime">3:30pm</div>
+                    <div class="btn-showTime">5:45pm</div>
+                    <div class="btn-showTime">7:30pm</div>
+                    
+            </div>
+            </div>
+            <div class="product">
+                <div class="prod-image-three"></div>
+                <div class="prod-desc">
+                    <h4>The 400 Blows (1959)</h4>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad accusantium vero, voluptates dolore rem iusto culpa! Eveniet corrupti dolore doloribus porro distinctio at nulla nesciunt iusto quae eaque. Aspernatur iste quisquam mollitia minima explicabo earum, delectus veritatis, reiciendis aperiam quo magnam distinctio beatae, nobis cupiditate iusto sed optio maiores eius.</p>
+                    <h4>SHOWTIMES:</h4>
+                    <div class="btn-showTime">12:00pm</div>
+                    <div class="btn-showTime">3:30pm</div>
+                    <div class="btn-showTime">5:45pm</div>
+                    <div class="btn-showTime">7:30pm</div>
+                    
+            </div>
+            </div>
+            
+        
+    </div>
+</div>`;
+
+var aboutContent = ` <div class="content-about">
+<div class="heroimg"></div>
+<h1>LOREM IPSUM THEATER:</h1>
+<div class="products">
+    <div class="product">
+        <div class="prod-image"></div>
+        <div class="prod-desc">
+            <h4>ABOUT:</h4>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad accusantium vero, voluptates dolore rem iusto culpa! Eveniet corrupti dolore doloribus porro distinctio at nulla nesciunt iusto quae eaque. Aspernatur iste quisquam mollitia minima explicabo earum, delectus veritatis, reiciendis aperiam quo magnam distinctio beatae, nobis cupiditate iusto sed optio maiores eius.</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad accusantium vero, voluptates dolore rem iusto culpa! Eveniet corrupti dolore doloribus porro distinctio at nulla nesciunt iusto quae eaque. Aspernatur iste quisquam mollitia minima explicabo earum, delectus veritatis, reiciendis aperiam quo magnam distinctio beatae, nobis cupiditate iusto sed optio maiores eius.</p>
+        </div>
+    </div>
+    <div class="fnw-text">
+        <h3>FRENCH NEW WAVE DIRECTORS</h3>
+    </div>
+    <div class="fnw-images">
+        <div class="fnw-image-one">
+            <div class="img-text">Jean-Luc Godard</div>
+        </div>
+        <div class="fnw-image-two">
+            <div class="img-text">Claude Chabrol</div>
+        </div>
+        <div class="fnw-image-three">
+            <div class="img-text">François Truffaut</div>
+        </div>
+    </div>
+</div>
+</div> `
+
+var shopContent = ` <div class="content-shop">
+<div class="heroimg"></div>
+<h1>SHOP THE COLLECTION:</h1>
+<div class="products">
+    <div class="product">
+        <div class="fnw-images-one">
+            <div class="fnw-image-one">
+                <div class="btnBuy">Buy Now</div>
+            </div>
+            <div class="fnw-image-two">
+                <div class="btnBuy">Buy Now</div>
+            </div>
+            <div class="fnw-image-three">
+                <div class="btnBuy">Buy Now</div>
+            </div>
+        </div>
+        <div class="fnw-images-two">
+            <div class="fnw-image-one">
+                <div class="btnBuy">Buy Now</div>
+            </div>
+            <div class="fnw-image-two">
+                <div class="btnBuy">Buy Now</div>
+            </div>
+            <div class="fnw-image-three">
+                <div class="btnBuy">Buy Now</div>
+            </div>
+        </div>
+        <div class="fnw-images-three">
+            <div class="fnw-image-one">
+                <div class="btnBuy">Buy Now</div>
+            </div>
+            <div class="fnw-image-two">
+                <div class="btnBuy">Buy Now</div>
+            </div>
+            <div class="fnw-image-three">
+                <div class="btnBuy">Buy Now</div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>`
+
+var contactContent = ` <div class="content-contact">
+<div class="heroimg"></div>
+<h1>CONTACT US:</h1>
+<div class="products">
+    <div class="product-form">
+        <div class="form-text-input">
+            <input type="text" placeholder="Your name..." />
+        </div>
+        <div class="form-text-input">
+            <input type="text" placeholder="Your Email..." />
+        </div>
+        <div class="form-text-message-input">
+            <input type="text" placeholder="Message..." />
+        </div>
+        <div class="btn-message">SEND MESSAGE</div>
+    </div>
+    </div> 
+</div>  `
+
+// this function is being called from the app.js and it has the page name
+export function modelPageName(pgName) {
+    console.log(pgName);
+
+    $("#app").html(eval(pgName));
+}
